9fcde36f17fe04c146c95c77586e3bb15275fd9e
Lu Bohong <20231198@buaa.edu.cn>
2022-03-13 20:09:22 +0800

    finish lab0.2

--- ray/sh_test1/changefile.sh
+++ ray/sh_test1/changefile.sh
@@ -4,10 +4,11 @@ while [ $a -le 100 ]
 do
 	if [ $a -gt 70 ]	   #if loop variable is greater than 70
 	then
-
-	elif [ $a -gt 40 ]         # else if loop variable is great than 40   
+		rm file$a -r
+	elif [ $a -gt 40 ]    # else if loop variable is great than 40
 	then
-	
+		mv file$a newfile$a
 	fi
 	           #don't forget change the loop variable
+	a=`expr $a + 1`
 done
