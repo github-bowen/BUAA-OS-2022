07768dc615ee595d1ad76466f9fb5bf0b84e0b7a
Lu Bohong <20231198@buaa.edu.cn>
2022-03-31 16:22:10 +0800

    x

--- my_cal/my_cal.c
+++ my_cal/my_cal.c
@@ -1,5 +1,9 @@
 #include "my_driver.S"
 
+char _ my_getchar();
+void _my_put_char(char);
+
+
 void my_cal() {
 	char c;
 	int ten = 0;
@@ -12,7 +16,7 @@ void my_cal() {
 
 	while (ten > 0) {
 		binary[i++] = ten % 2;
-		tem /= 2;
+		ten /= 2;
 	}
 	i--;
 	while (i >= 0) {
