16f122196c073828063acf00be81a3f79eaec381
Lu Bohong <20231198@buaa.edu.cn>
2022-04-28 08:49:56 +0800

    commit by bash

--- lib/env.c
+++ lib/env.c
@@ -191,7 +191,7 @@ env_setup_vm(struct Env *e)
 	e->env_pgdir = pgdir;
 	e->env_cr3 = PADDR(pgdir);
     /* UVPT maps the env's own page table, with read-only permission.*/
-	// e->env_pgdir[PDX(VPT)] = e->env_cr3;
+	e->env_pgdir[PDX(VPT)] = e->env_cr3;
     e->env_pgdir[PDX(UVPT)] = e->env_cr3 | PTE_V;
     return 0;
 }
