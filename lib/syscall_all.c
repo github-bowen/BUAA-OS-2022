e76fe9107159e4846db5b359f81a4a561b100d2d
Lu Bohong <20231198@buaa.edu.cn>
2022-05-19 15:35:57 +0800

    commit by bash

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -324,8 +324,16 @@ void sys_panic(int sysno, char *msg)
 void sys_ipc_recv(int sysno, u_int dstva)
 {
     if (dstva >= UTOP) return;
-    if (!sender) sender->env_status = ENV_RUNNABLE;
-    sys_yield();
+    LIST_FOREACH(m, &msgs, q_link) {
+		if (m->r_id == curenv->env_id) {
+			r->env_status = ENV_RUNNALBLE;
+			curenv->env_ipc_recving = 1;
+		    curenv->env_ipc_dstva = dstva;
+	    	curenv->env_status = ENV_NOT_RUNNABLE;	
+			sys_yield();
+		}
+	}
+
     curenv->env_ipc_recving = 1;
     curenv->env_ipc_dstva = dstva;
     curenv->env_status = ENV_NOT_RUNNABLE;
